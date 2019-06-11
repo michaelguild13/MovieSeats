0c97326585305eede26bad8a9da64ee54d85a31f
Michael Guild <michaelguild13@gmail.com>
2019-06-11 17:02:53 -0400

    404 catch

--- src/api.js
+++ src/api.js
@@ -9,4 +9,10 @@ API.get('/', (req, res) => {
   res.json(DATA)
 })
 
+// Handle 404
+API.use(function(req, res) {
+    res.status(404)
+    res.json({})
+});
+
 export default API
--- src/api.test.js
+++ src/api.test.js
@@ -2,10 +2,15 @@ import request from 'supertest'
 import API from './api'
 import data from './data'
 
-describe('Test the root path', () => {
-  test('It should response the GET method', async () => {
-      const { status, body} = await request(API).get('/')
-      expect(status).toBe(200)
-      expect(‎JSON.stringify(body)).toMatch(JSON.stringify(data))
-  });
+describe('Test API', () => {
+  it('should GET all the data at root', () => {
+      return request(API)
+              .get('/')
+              .expect(200, data)
+  })
+  it('should fail on post', () => {
+    return request(API)
+            .post('/')
+            .expect(404, {})
+  })
 })
